63d2f65790f153ee0e3ed0a322c23dde86482b2a
amanmehta-maniac <amanmehta1997@gmail.com>
2017-03-22 15:23:23 +0530

    index command added

--- ser/server.py
+++ ser/server.py
@@ -1,6 +1,23 @@
 import os,re,socket,time
 
 
+def index_handle(comm):
+	ls = os.popen("ls -l").read()
+	ls = ls.split("\n")
+	ls.remove(ls[0])
+	# ls = ls.split()
+	# ls[0] contains 1st line of ls -l output
+	output=""
+	if comm[1]=="longlist":
+		for x in xrange(0,len(ls)-1):
+			output = output + ls[x] + "\n"
+	if comm[1]=="shortlist":
+		for x in xrange(0,len(ls)-1):
+			l = comm[2] + " " + comm[3] + " " + comm[4] 
+			r = comm[5] + " " + comm[6] + " " + comm[7]
+			 
+
+
 port = 60000
 s = socket.socket()
 host = ""
@@ -13,7 +30,9 @@ print "Server is up and listening!"
 ip,addr=s.accept()
 while True:
 	data = ip.recv(1024)
-	data = data.
+	data = data.split(" ")
+	if data[0]=="index":
+		index_handle(data)
 
 
 
